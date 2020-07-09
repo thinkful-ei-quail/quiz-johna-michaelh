8d08366e60c03971422281b16b0ff2834beda94f
John Adney <jmoon5ftm@gmail.com>
2020-07-09 13:38:08 -0700

    halfway-working

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 "use strict";
 
-
 const STATE_WELCOME = 0;
 const STATE_UNANSWERED = 1;
-const STATE_RIGHT = 2;
-const STATE_WRONG = 3;
-const STATE_RESULTS = 4;
-
-
+const STATE_ANSWERED = 2;
+const STATE_RESULTS = 3;
 
 const createQuestion = (question, answers, rightAnswer)=>{
   return{
@@ -16,63 +12,59 @@ const createQuestion = (question, answers, rightAnswer)=>{
     rightAnswer,
   };
 };
-const store = {
 
-  questions:[ createQuestion(
-    
-    'Where did Katara meet Aang?',
-    [
-      'South Pole',
-      'Kyoshi Island',
-      'Whale Tail Island',
-      'Ba Sing Se',
-    ],
-    'South Pole'
-  ),
-  createQuestion(
-    
-    'Which teacher helped Aang master fire bending?',
-    [
-      'Jeong Jeong',
-      'Iroh',
-      'The Twin Dragons',
-      'Zuko',
-    ],
-    'Zuko'
-  ),
-  createQuestion(
-    
-    'Who taught Toph to earth bend?',
-    [
-      'Master Yu',
-      'The Badger Moles',
-      'Xin Fu',
-      'King Bumi',
-    ],
-    'The Badger Moles'
-  ),
-  createQuestion(
-    
-    'In the Avatar cycle, What element follows water?',
-    [
-      'Earth',
-      'Fire',
-      'Air',
-      'Lightning',
-    ],
-    'Earth'
-  ),
-  createQuestion(
-    
-    "Why couldn't Iroh teach Zuko to create lightning?",
-    [
-      'Zuko could not create lightning',
-      "Zuko's heart was not pure enough to use lightning",
-      "Zuko's inner turmoil made him too tense to create lightning",
-      'Iroh did not know how to use lightning',
-    ],
-    "Zuko's inner turmoil made him too tense to create lightning"
-  ),
+const store = {
+  questions:[
+    createQuestion(
+      'Where did Katara meet Aang?',
+      [
+        'South Pole',
+        'Kyoshi Island',
+        'Whale Tail Island',
+        'Ba Sing Se',
+      ],
+      'South Pole'
+    ),
+    createQuestion(    
+      'Which teacher helped Aang master fire bending?',
+      [
+        'Jeong Jeong',
+        'Iroh',
+        'The Twin Dragons',
+        'Zuko',
+      ],
+      'Zuko'
+    ),
+    createQuestion(    
+      'Who taught Toph to earth bend?',
+      [
+        'Master Yu',
+        'The Badger Moles',
+        'Xin Fu',
+        'King Bumi',
+      ],
+      'The Badger Moles'
+    ),
+    createQuestion(
+      'In the Avatar cycle, What element follows water?',
+      [
+        'Earth',
+        'Fire',
+        'Air',
+        'Lightning',
+      ],
+      'Earth'
+    ),
+    createQuestion(
+      "Why couldn't Iroh teach Zuko to create lightning?",
+      [
+        'Zuko could not create lightning',
+        "Zuko's heart was not pure enough to use lightning",
+        "Zuko's inner turmoil made him too tense to create lightning",
+        'Iroh did not know how to use lightning',
+      ],
+      "Zuko's inner turmoil made him too tense to create lightning"
+    ),
   ],
   currentState: STATE_WELCOME,
   currentQuestionNumber: 0,
@@ -81,61 +73,72 @@ const store = {
   currentAnswer: 0,
 };
 
+const generateWelcomeHtml = () => '<button class="start-button">Start</button>';
 
+const generateQuestionHeaderHtml = (currentQuestionNumber, questionCount, numberRight, numberWrong) => `
+  <span>${currentQuestionNumber + 1} of ${questionCount}</span>
+  <span>${numberWrong} wrong ${numberRight} right</span>
+`;
+
+const generateQuestionHtml = (questionText, answerTexts, rightIndex, wrongIndex) => {
+  let classNames = [ '', '', '', '' ];
+  if(rightIndex)
+    classNames[rightIndex] = "right-answer";
+  if(wrongIndex)
+    classNames[wrongIndex] = "wrong-answer";
+  return `<p>${questionText}${rightIndex ? '<button id="next-button">Next --></button>' : ''}</p>
+     <form id="q-form">
+         <input class="${classNames[0]}" type="submit" value="${answerTexts[0]}"></input>
+         <input class="${classNames[1]}" type="submit" value="${answerTexts[1]}"></input>
+         <input class="${classNames[2]}" type="submit" value="${answerTexts[2]}"></input>
+         <input class="${classNames[3]}" type="submit" value="${answerTexts[3]}"></input>
+     </form>`;
+};
+
+const $bodyElement = $('body'), $headerElement = $('header'), $mainElement = $('main');
 
 const render = () =>{
   switch(store.currentState){
-  case STATE_WELCOME:
-    //replace main with a start button
-    break;
-  case STATE_UNANSWERED:
-    //replace main with a question and four answers, add question header, and hide the h1
-    break;
-  case STATE_RIGHT:
-    //color green  and disable the answer buttons and add a next button
-    break;
-  case STATE_WRONG:
-    //color red and disable the answer buttons and add a next button
-    break;
-  case STATE_RESULTS:
-    //replace main with a results message and add a try again button
-    break; 
+    case STATE_WELCOME:
+      $bodyElement.attr('class', 'welcome-screen');
+      $mainElement.html(generateWelcomeHtml());
+      break;
+    case STATE_UNANSWERED:
+      $bodyElement.attr('class', 'question-screen');
+      let q = store.questions[store.currentQuestionNumber];
+      $headerElement.attr('class', 'question-header');
+      $headerElement.html(
+        generateQuestionHeaderHtml(store.currentQuestionNumber, store.questions.length, store.numberRight, store.numberWrong));
+      $mainElement.html(generateQuestionHtml(q.question, q.answers));
+      break;
+    case STATE_ANSWERED:
+      //color and disable the answer buttons and add a next button
+      break;
+    case STATE_RESULTS:
+      //replace main with a results message and add a try again button
+      break; 
   }
 };
 
-const generateQuestionHTML = (questionText, answerTexts ) => {
-  return `<p>${questionText}</p>
-     <div>
-         <button>${answerTexts[0]}</button>
-         <button>${answerTexts[0]}</button>
-         <button>${answerTexts[0]}</button>
-         <button>${answerTexts[0]}</button>
-     </div>`;
+const onStart = e => {
+  console.log("onStart!");
+  store.currentQuestionNumber = 0;
+  store.numberRight = store.numberWrong = 0;
+  store.currentState = STATE_UNANSWERED;
+  render();
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+const onAnswerSelected = e => {
+  // check whether the answer is right and move to 
+};
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+const onNextQuestion = e => {
+  // move to next question and unanswered state
+};
 
-// These functions return HTML templates
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-/********** EVENT HANDLER FUNCTIONS **********/
-
-// These functions handle events (submit, click, etc)
+$(() => {
+  render();
+  $mainElement.on('click', '.start-button', onStart);
+  $mainElement.on('submit', '.q-form', onAnswerSelected);
+  $mainElement.on('click', '#next-button', onNextQuestion);
+});
