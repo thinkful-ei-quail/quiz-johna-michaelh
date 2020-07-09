2e2a30c5dea6e41e6a284c78177fdccbc51a7dc1
Eclipse <lpskirito7@outlook.com>
2020-07-09 12:45:58 -0400

    started javascript

--- app.js
+++ app.js
@@ -1,34 +1,117 @@
-/**
- * Example store structure
- */
+"use strict";
+
+
+const STATE_WELCOME = 0;
+const STATE_UNANSWERED = 1;
+const STATE_RIGHT = 2;
+const STATE_WRONG = 3;
+const STATE_RESULTS = 4
+
+
+
+const createQuestion = (question, answers, rightAnswer)=>{
+  return{
+    question,
+    answers,
+    rightAnswer,
+   }
+}
 const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+
+ questions:[ createQuestion(
+    
+     'Where did Katara meet Aang?',
+       [
+        'South Pole',
+        'Kyoshi Island',
+        'Whale Tail Island',
+        'Ba Sing Se',
+       ],
+       'South Pole',
+    ),
+   createQuestion(
+    
+     'Which teacher helped Aang master fire bending?',
+       [
+        'Jeong Jeong',
+        'Iroh',
+        'The Twin Dragons',
+        'Zuko',
+       ],
+       'Zuko',
+    ),
+ createQuestion(
+    
+     'Who taught Toph to earth bend?',
+       [
+        'Master Yu',
+        'The Badger Moles',
+        'Xin Fu',
+        'King Bumi',
+       ],
+       'The Badger Moles',
+    ),
+   createQuestion(
+    
+     'In the Avatar cycle, What element follows water?',
+       [
+        'Earth',
+        'Fire',
+        'Air',
+        'Lightning',
+       ],
+       'Earth',
+    ),
+    createQuestion(
+    
+     "Why couldn't Iroh teach Zuko to create lightning?",
+       [
+        'Zuko could not create lightning',
+        "Zuko's heart was not pure enough to use lightning",
+        "Zuko's inner turmoil made him too tense to create lightning",
+        'Iroh did not know how to use lightning',
+       ],
+       "Zuko's inner turmoil made him too tense to create lightning",
+    ),
       ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
+  currentState: STATE_WELCOME,
+  currentQuestionNumber: 0,
+  numberRight: 0,
+  numberWrong: 0,
+  currentAnswer: 0,
 };
+
+
+
+const render = () =>{
+  switch(store.currentState){
+    case STATE_WELCOME:
+      //replace main with a start button
+    break;
+    case STATE_UNANSWERED:
+      //replace main with a question and four answers, add question header, and hide the h1
+    break;
+    case STATE_RIGHT:
+      //color green  and disable the answer buttons and add a next button
+    break;
+    case STATE_WRONG:
+      //color red and disable the answer buttons and add a next button
+    break;
+    case STATE_RESULTS:
+      //replace main with a results message and add a try again button
+    break; 
+  }
+}
+
+const generateQuestionHTML = (questionText, answerTexts, ) => {
+     return `<p>${questionText}</p>
+     <div>
+         <button>${answerTexts[0]}</button>
+         <button>${answerTexts[0]}</button>
+         <button>${answerTexts[0]}</button>
+         <button>${answerTexts[0]}</button>
+     </div>`
+}
 
 /**
  * 
